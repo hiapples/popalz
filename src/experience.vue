551d8ad20e1d52a98f466340c06ab84bec141a9a
hiapples <howard.2001@yahoo.com.tw>
2024-08-25 01:34:04 +0800

    6

--- src/experience.vue
+++ src/experience.vue
@@ -7,6 +7,29 @@
     <div class="experience mb-5">
         <div>Experience</div>
     </div>
+    <div class="row">
+        <div class="col-sm-4 mt-4">
+            <div class="card">
+                <div class="card-body">
+                    <p class="card-text">我喜歡撰寫程式碼，<br/>享受從中解決問題並提升效率的過程。<br/>對我來說，寫程式不僅是一項技能，<br/>更是一種創造力的展現。<br/>凡事都會全力以赴，<br/>確保每個細節都達到最佳狀態。</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 mt-4">
+            <div class="card">
+                <div class="card-body">
+                    <p class="card-text">我喜歡撰寫程式碼，<br/>享受從中解決問題並提升效率的過程。<br/>對我來說，寫程式不僅是一項技能，<br/>更是一種創造力的展現。<br/>凡事都會全力以赴，<br/>確保每個細節都達到最佳狀態。</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 mt-4">
+            <div class="card">
+                <div class="card-body">
+                    <p class="card-text">我喜歡撰寫程式碼，<br/>享受從中解決問題並提升效率的過程。<br/>對我來說，寫程式不僅是一項技能，<br/>更是一種創造力的展現。<br/>凡事都會全力以赴，<br/>確保每個細節都達到最佳狀態。</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
@@ -18,5 +41,19 @@
         display: flex;
         justify-content: center;
         border-radius: 15px;
+    }
+    .card-body{
+        color: white;
+        text-align: center;
+        padding: 30px;
+    }
+    .card{
+        background-color: rgb(23, 22, 22);
+    }
+    .card-text{
+        font-size: 14px;
+    }
+    p {
+        line-height: 2; 
     }
 </style>
