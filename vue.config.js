116cf4ecacf9518e0f7d614ce23ea0f80f2223ee
hiapples <howard.2001@yahoo.com.tw>
2024-08-24 20:11:43 +0800

    5

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,19 @@ module.exports = {
     outputDir: 'dist',
     devServer: {
       proxy: 'http://localhost:8000'  // PHP 服务器的地址
+    },
+    pwa: {
+      name: '我的 PWA 应用',
+      themeColor: '#4DBA87',
+      msTileColor: '#000000',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+      manifestOptions: {
+        short_name: 'PWA',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#42b983'
+      }
     }
   }
   
