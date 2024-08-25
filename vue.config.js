68a24fab00fc603ff01d25bdb8f4b5d03b51ce47
hiapples <howard.2001@yahoo.com.tw>
2024-08-25 19:23:51 +0800

    6

--- vue.config.js
+++ vue.config.js
@@ -6,10 +6,10 @@ module.exports = {
   },
   pwa: {
     name: 'PoPalz',
-    themeColor: '#000000',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
+    themeColor: '#000000',//浏览器工具栏的主题颜色
+    msTileColor: '#000000',//Windows 设备上磁贴的背景颜色
+    appleMobileWebAppCapable: 'yes',//全屏模式显示
+    appleMobileWebAppStatusBarStyle: 'black',//设置 iOS 设备上的状态栏的样式颜色
     workboxOptions: {
       // skip waiting on the new service worker
       skipWaiting: true,
