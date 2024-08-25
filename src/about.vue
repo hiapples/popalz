5de1f8b7f1991af2355f4956027e24ddd2ac326c
hiapples <howard.2001@yahoo.com.tw>
2024-08-25 23:24:50 +0800

    6

--- src/about.vue
+++ src/about.vue
@@ -2,6 +2,24 @@
     import { ref } from "vue";
     import { onMounted,onUpdated,onUnmounted } from "vue";
     import 'bootstrap/dist/css/bootstrap.min.css'; // 引入 Bootstrap CSS
+    const fadeInElement = ref(null);
+    const fadeInElement2 = ref(null);
+    onMounted(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('fade-in');
+                }
+            });
+        });
+
+        if (fadeInElement.value) {
+            observer.observe(fadeInElement.value);
+        }
+        if (fadeInElement2.value) {
+            observer.observe(fadeInElement2.value);
+        }
+    });
 </script>
 <template>
     <div class="about mb-5">
@@ -9,8 +27,8 @@
     </div>
     <div class="container">
         <div class="mb-5">
-            <div class="card">
-                <div class="card-body">
+            <div class="card ">
+                <div class="card-body" ref="fadeInElement">
                     <h5 class="card-title">韋皓承</h5>
                     <p class="card-text">2001/01/16</p> 
                     <p class="card-text mt-3">國立東華大學資訊管理系</p>
@@ -30,7 +48,7 @@
         </div>
         <div class="mt-5">
             <div class="card">
-                <div class="card-body">
+                <div class="card-body" ref="fadeInElement2">
                     <p class="card-text">我喜歡撰寫程式碼，<br/>享受從中解決問題並提升效率的過程。<br/>對我來說，寫程式不僅是一項技能，<br/>更是一種創造力的展現。<br/>凡事都會全力以赴，<br/>確保每個細節都達到最佳狀態。</p>
                 </div>
             </div>
@@ -58,6 +76,8 @@
         color: white;
         text-align: center;
         padding: 30px;
+        opacity: 0; /* Initially hidden */
+        transform: translateY(-20px); /* Start position for animation */
     }
     .card{
         background-color: rgb(23, 22, 22);
@@ -67,5 +87,18 @@
     }
     p {
         line-height: 2; 
+    }   
+    .fade-in {
+        animation: fade-in 1.5s ease-in-out forwards;
+    }
+    @keyframes fade-in {
+        from {
+            opacity: 0;
+            transform: translateY(-20px);
+        }
+        to {
+            opacity: 1;
+            transform: translateY(0);
+        }
     }
 </style>
