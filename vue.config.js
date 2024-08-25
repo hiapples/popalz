54064f50b172c2692f1fe076ee1be20158436a8c
hiapples <howard.2001@yahoo.com.tw>
2024-08-25 19:11:52 +0800

    6

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,18 @@ module.exports = {
   outputDir: 'dist',
   devServer: {
     proxy: 'http://localhost:8000'  // PHP 服务器的地址
+  },
+  pwa: {
+    name: 'PoPalz',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxOptions: {
+      // skip waiting on the new service worker
+      skipWaiting: true,
+      clientsClaim: true,
+    }
   }
 }
   
